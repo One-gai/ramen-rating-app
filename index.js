15d986bc38e5cfb1820e00aea050059eb530b9c2
Luke <luke.wangai@student.moringaschool.com>
2025-03-18 23:02:46 +0300

    stage 4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const ramens = [
     { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
     { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "kojiro.jpg", rating: 4, comment: "Very flavorful!" },
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "gyukotsu.jpg" },
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "gyukotsu.jpg", rating: 4, comment: "Very Chic and Tasty!" },
     { id: 4, name: "Naruto ramen", restaurant: "Ichiraku Ramen", image: "naruto.jpg", rating: 4, comment: "Very Filling!" },
     { id: 5, name: "Nirvana ramen", restaurant: "Nirvana Hotel", image: "nirvana.jpg", rating: 4, comment: "Very Spicy!" },
  ];
@@ -11,45 +11,46 @@ const ramens = [
    ramens.forEach(ramen => {
       const imageElement=document.createElement("img");
       imageElement.src = ramen.image;
-      imageElement.alt = ramen.alt
+      imageElement.alt = ramen.name
       imageElement.dataset.id = ramen.id;
-      imageElement.addEventListener('click', handleclick)
+      imageElement.addEventListener('click', handleClick)
       ramenDetails.appendChild(imageElement)
 
    }
    )
  }
    
- function handleclick(Rapid){
+ function handleClick(Rapid){
    const ramenId = Rapid.target.dataset.id;
    const ramen = ramens.find (r => r.id == ramenId);
-   const myImage = document.getElementById ("ImageDisplay");
+   const myImage = document.getElementById ("ramenImages");
    myImage.src = ramen.image;
    myImage.alt = ramen.name 
    myImage.style.display = "block"
    
-   document.getElementById ("nameRamen").value
-   document.getElementById ("kitchenMade").value
-   document.getElementById ("ramenPodium").value
-   document.getElementById ("commentSection").value
+   document.getElementById ("nameRamen").textContent = ramen.name
+   document.getElementById ("kitchenMade").textContent = ramen.restaurant
+   document.getElementById ("ramenPodium").textContent = ramen.rating
+   document.getElementById ("commentSection").textContent = ramen.comment
 }
  function addSubmitListener(){
    const form = document.getElementById("ramenForm")
-   form.addEventListener('submit'),(e)=> {
-     e.preventDefault()
+   form.addEventListener("submit", (event) => {
+     event.preventDefault();
 
      const name = document.getElementById("ramenFormInput").value;
       const restaurant = document.getElementById("ramenResturantInput").value;
       const rating = document.getElementById("ramenRatingInput").value;
       const comment = document.getElementById("ramenCommentInput").value;
       const img = document.createElement("img");
-      img.src = ramens  
+      img.src = document.getElementById("ramenUrlInput").value
       img.alt = `${name} from ${restaurant}`;
-      document.getElementById("ramenSelection").appendChild("");
+      document.getElementById("ramenSelection").appendChild(img);
+ });
  }
-}
 
  function main(){
-   displayRamens()
+   displayRamens();
+   addSubmitListener();
  }
  document.addEventListener('DOMContentLoaded', main)
